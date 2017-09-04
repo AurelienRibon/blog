9f31199ccb8cd6438520637d33078083c0fedd72
Aurelien Ribon <aurelien.ribon@gmail.com>
2017-09-04 10:09:31 +0200

    Bind server to envvar PORT provided by heroku

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const db      = require('./db');
 
-const PORT      = 4000;
+const PORT      = process.env.PORT || 4000;
 const DATA_HOST = 'http://aurelienjp.cluster010.ovh.net/data/blog';
 
 setupAndStart();
